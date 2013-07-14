0c129f917c62c0518abea2df51104e6c634f80aa
mrahman <rahmanww@gmail.com>
2013-07-14 14:50:43 -0400

    project road map

---
+++ src/student/useapp/UseStudentApp.java
@@ -0,0 +1,15 @@
+package student.useapp;
+
+public class UseStudentApp {
+
+	/**
+	 * This is the main method of using student application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(" <<<<<<<<..Welcome to Student Application, Feel free to Use it..>>>>>>>");
+
+	}
+
+}
