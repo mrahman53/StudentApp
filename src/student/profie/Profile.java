2fca224dd12cd208aef8dab16c0d6fe70f0a13ab
mrahman <rahmanww@gmail.com>
2013-07-19 22:06:18 -0400

    profile is added

---
+++ src/student/profie/Profile.java
@@ -0,0 +1,24 @@
+package student.profie;
+
+public class Profile {
+	
+	String studentName;
+	int studentId;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+	
+	
+
+}
