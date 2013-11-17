0849a9aab5f94e71ffeb160cc059a9fbcf352919
mdmkhan1982 <mdmkhan1982@gmail.com>
2013-11-17 18:41:06 -0500

    delete

--- HomeWork4/src/employeeInfo/EmployeeName.java
+++
@@ -1,7 +0,0 @@
-package employeeInfo;
-
-public class EmployeeName extends DepartmentsName {// Inheritance child class
-	public void employeeName(){
-		System.out.println("Employee Name is : MD Mamun Khan");
-		}
-} 
